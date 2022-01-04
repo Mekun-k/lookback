d4d9e255c4bb6b7f587e650a1d1fc6f497fe32e3
Mekun-k <akaidonnbei@gmail.com>
2022-01-05 06:11:10 +0900

    Add: usernameエラーのため追加

--- db/migrate/20220104080825_devise_create_users.rb
+++ db/migrate/20220104080825_devise_create_users.rb
@@ -34,6 +34,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
 
 
       t.timestamps null: false
+
+      t.string :username, null: false
     end
 
     add_index :users, :email,                unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_01_04_080825) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
